b3ad43a1b97fb11bcdcf70f4716d3812b5d1122b
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2025-04-08 12:26:16 +0700

    Update mapfontstv.js

--- mapfontstv.js
+++ mapfontstv.js
@@ -1,1 +1,10 @@
+const charMap = {
+    '': 'T',
+    '': 'E',
+    '': 'S',
+    // thêm các ký tự khác nếu có
+};
 
+function replaceMappedChars(str) {
+    return str.split('').map(char => charMap[char] || char).join('');
+}
